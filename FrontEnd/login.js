e6d26a1b923f27623ad3f40b9ded030340af5dc3
Alexandre <alexmiquel888@gmail.com>
2024-06-09 10:41:28 +0200

    Page de connexion
    Gestion de la connexion et des messages d'erreur +renvoie sur la page index.html

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -5,8 +5,8 @@ document.addEventListener('DOMContentLoaded', () => {
     loginForm.addEventListener('submit', async (event) =>{
         event.preventDefault();
 
-        const email = document.getElementById('email');
-        const password = document.getElementById('password');
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
 
         //On créer un objet avec les données de connexion
         const loginData = {
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         try{
             //On envoie une requête POST à l'API d'authentification
-            const reponse = await fetch("http://localhost:5678/users/login", {
+            const reponse = await fetch("http://localhost:5678/api/users/login", {
                 method: 'POST',
                 headers: {
                     //On specifie que les données sont au format json
@@ -25,23 +25,24 @@ document.addEventListener('DOMContentLoaded', () => {
                 //On converti l'objet loginData en JSON
                 body: JSON.stringify(loginData)
             });
+            console.log('Reponse status:', reponse.status); // Log the status
         
             if(reponse.ok){//Verifie si la réponse est réussie
-                const data = await Response.json();//On extrait les données JSON de la réponse
+                const data = await reponse.json();//On extrait les données JSON de la réponse
                 localStorage.setItem('authToken', data.token);//On stock le token dans le localStorage pour de futur utilisation
-                window.location.href = "FrontEnd/index.html";//Si la connexion réussi on amène l'utilisateur sur la page d'accueil
+                window.location.href = "index.html";//Si la connexion réussi on amène l'utilisateur sur la page d'accueil
             }else{
-                const error = await Response.json;//On extrait les données de l'erreur 
-                if(response.status === 404){
+                const error = await reponse.json;//On extrait les données de l'erreur 
+                if(reponse.status === 404){
                     displayErrorMessage('Utilisateur non trouvé.'); // Message spécifique pour l'erreur 404
-                }else if(response.status === 401){
+                }else if(reponse.status === 401){
                     displayErrorMessage('Email ou mot de passe incorrect.'); // Message spécifique pour l'erreur 401
                 }else{
                     displayErrorMessage(error.message); //Message d'erreur de base 
                 }
             }
         }catch(error){
-            displayErrorMessage('Une erreur est survenue. Veuillez réessayer plus tard.'); // on affiche un message d'erreur générique en cas de problème quelconque
+            displayErrorMessage('Une erreur est survenue. Veuillez réessayer plus tard.'); // on affiche un message d'erreur générique si la requête à l'API échoue
         }
     });
      // Fonction pour afficher un message d'erreur
