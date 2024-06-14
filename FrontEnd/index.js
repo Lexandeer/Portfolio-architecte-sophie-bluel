62ecd73bc0400b44176aeaa09e7e7ee223681a8c
Alexandre <alexmiquel888@gmail.com>
2024-06-14 12:48:35 +0200

    modification fichier + ajout de projet via modal
    Gestion de l'ajout de projet via la modale et déplacement des fonctions index.js -> utils.js

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,5 +1,5 @@
 //On importe les fonctions depuis utils.js
-import { fetchProjets, fetchCategories, ajouterCategories, ajouterProjets } from './utils.js'
+import { fetchProjets, fetchCategories, ajouterCategories, ajouterProjets, afficherEtat1} from './utils.js'
 
 
 // Vérifie si l'utilisateur est connecté en vérifiant la présence du token dans le localStorage
@@ -112,250 +112,33 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 
 
-// Boîte modale :
+// Boîte modale
+document.addEventListener('DOMContentLoaded', async () => {
+    const openModalBtn = document.querySelector('a[href="#modal1"]');
+    const modal = document.getElementById('modal1');
+    const modalWrapper = modal.querySelector('.modal-wrapper');
+    const modalContent = modal.querySelector('.modal-content');
+    const closeModalBtn = modalWrapper.querySelector('.js-modal-close');
+    const addPictureBtn = modalWrapper.querySelector('.js-add-picture');
+    const backModalBtn = modalWrapper.querySelector('.js-modal-back');
 
-const openModalBtn = document.querySelector('a[href="#modal1"]');
-const modal = document.getElementById('modal1');
-const modalWrapper = modal.querySelector('.modal-wrapper');
-const modalContent = modal.querySelector('.modal-content');
-const closeModalBtn = modalWrapper.querySelector('.js-modal-close');
-const addPictureBtn = modalWrapper.querySelector('.js-add-picture');
-const backModalBtn = modalWrapper.querySelector('.js-modal-back');
-    
-//On affiche la modal
-openModalBtn.addEventListener('click', (event) => {
-    event.preventDefault();
-    modal.style.display = 'flex';
-    afficherEtat1()
-});
+    openModalBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        modal.style.display = 'flex';
+        afficherEtat1(modalContent, addPictureBtn, backModalBtn);
+    });
 
-//On ferme la modale au clique sur la croix
-closeModalBtn.addEventListener('click', () => {
-    modal.style.display = 'none'; // Cache la modale
-});
-
-// Retour à l'état 1 de la modale
-backModalBtn.addEventListener('click', () => {
-    afficherEtat1()
-});
-
- //on ferme la modale en cliquant en dehors de la modale et on change le display
- window.addEventListener('click', (event) => {
-    if (event.target === modal) {
+    closeModalBtn.addEventListener('click', () => {
         modal.style.display = 'none';
-    }
+    });
+
+    backModalBtn.addEventListener('click', () => {
+        afficherEtat1(modalContent, addPictureBtn, backModalBtn);
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
 });
-
-// Fonction pour afficher l'état 1 de la modale
-function afficherEtat1() {
-    // On nettoie le contenu
-    modalContent.innerHTML = '';
-
-    // On ajoute le titre
-    const titre = document.createElement('h2');
-    titre.textContent = 'Galerie photo';
-    modalContent.appendChild(titre);
-
-    // On ajoute la div container pour les photos
-    const photosContainer = document.createElement('div');
-    photosContainer.classList.add('photos-container');
-    modalContent.appendChild(photosContainer);
-
-    // On ajoute les images des projets
-    fetchProjets().then(projets => {
-        projets.forEach(projet => {
-            const projetWrapper = document.createElement('div');
-            projetWrapper.classList.add('projet-wrapper');
-
-            const img = document.createElement('img');
-            img.src = projet.imageUrl;
-            img.alt = projet.title;
-
-            const deleteBtn = document.createElement('button');
-            deleteBtn.classList.add('delete-btn');
-            deleteBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
-
-            // Ajout de l'écouteur d'événement pour la suppression
-            deleteBtn.addEventListener('click', async () => {
-                await supprimerProjet(projet.id, projetWrapper);
-            });
-
-            projetWrapper.appendChild(img);
-            projetWrapper.appendChild(deleteBtn);
-            photosContainer.appendChild(projetWrapper);
-        });
-    });
-
-    // Mettre à jour le texte du bouton
-    addPictureBtn.textContent = 'Ajouter une photo';
-    // Cacher le bouton de retour
-    backModalBtn.style.display = 'none';
-
-    // On s'assure de ne pas avoir plusieurs écouteurs
-    addPictureBtn.removeEventListener('click', formSubmit);
-    addPictureBtn.addEventListener('click', afficherEtat2);
-}
-
-// Fonction pour afficher l'état 2 de la modale
-function afficherEtat2() {
-    // On nettoie le contenu
-    modalContent.innerHTML = '';
-
-    // On ajoute le titre
-    const titre = document.createElement('h2');
-    titre.textContent = 'Ajout photo';
-    modalContent.appendChild(titre);
-
-    // On affiche le bouton de retour
-    backModalBtn.style.display = 'block';
-   
-    // On ajoute le formulaire d'ajout de photo
-    const form = document.createElement('form');
-    form.classList.add('modal-form');
-    form.innerHTML = `
-        <div class="photo">
-            <i class="fa-regular fa-image"></i>
-            <label for="photo"><button>+ Ajouter photo</button></label>
-            <p>jpg, png : 4mo max</p>
-        </div>
-        <input type="file" id="photo" name="photo" accept="image/*">
-        <label for="title" class="label">Titre:</label>
-        <input type="text" id="title" name="title">
-        <label for="categorie" class="label">Catégorie:</label>
-        <select id="categorie" name="categorie">
-            <option value="">Sélectionnez une catégorie</option>
-        </select>
-        <p class="error-message" style="color:red;"></p>
-    `;
-    modalContent.appendChild(form);
-
-    // Récupérer les catégories et les ajouter au dropdown
-    fetchCategories().then(categories => {
-        const selectCategorie = form.querySelector('#categorie');
-        categories.forEach(categorie => {
-            const option = document.createElement('option');
-            option.value = categorie.id;
-            option.textContent = categorie.name;
-            selectCategorie.appendChild(option);
-        });
-    });
-
-    // Ajoutez un gestionnaire d'événement pour le bouton "Ajouter photo"
-    const addPhotoBtn = form.querySelector('.photo button');
-    addPhotoBtn.addEventListener('click', (event) => {
-        event.preventDefault();
-        form.querySelector('#photo').click();
-    });
-
-
-    // Mettre à jour le texte du bouton
-    addPictureBtn.textContent = 'Valider';
-
-    addPictureBtn.removeEventListener('click', afficherEtat2); 
-    addPictureBtn.addEventListener('click', (event) => {
-        event.preventDefault();
-        formSubmit(form);
-    });
-}
-
-// Gestion de la suppression des projets dans la modale(Etat1)
-async function supprimerProjet(projetId, projetWrapper) {
-    try {
-        const response = await fetch(`http://localhost:5678/api/works/${projetId}`, {
-            method: 'DELETE',
-            headers: {
-                'Authorization': `Bearer ${localStorage.getItem('authToken')}`
-            }
-        });
-
-        if (response.ok) {
-            // Supprimer l'élément de projet du DOM
-            projetWrapper.remove();
-            // Mettre à jour la liste des projets sur la page d'accueil
-            const projets = await fetchProjets();
-            ajouterProjets(projets);
-        } else {
-            console.error("Erreur lors de la suppression du projet:", await response.json());
-        }
-    } catch (error) {
-        console.error("Erreur lors de la suppression du projet:", error);
-    }
-}
-
-// Gestion du clic sur le bouton "Ajouter une photo" ou "Valider"
-function addPictureClick(event) {
-    event.preventDefault();
-    if (addPictureBtn.textContent === 'Ajouter une photo') {
-        afficherEtat2();
-    } else if (addPictureBtn.textContent === 'Valider') {
-        formSubmit(document.querySelector('.modal-form'));
-    }
-}
-
-// Gestion de la soumission du formulaire
-async function formSubmit(form) {
-    const formData = new FormData(form);
-    const errorMessage = form.querySelector('.error-message');
-    errorMessage.textContent = ''; // Réinitialise le message d'erreur
-
-    // On vérifie que tous les champs soient bien remplis 
-    if (!formData.get('photo') || !formData.get('title') || !formData.get('categorie')) {
-        errorMessage.textContent = 'Veuillez remplir tous les champs.';
-        return;
-    }
-
-    // Log des données du formulaire
-    console.log('FormData entries:');
-    formData.forEach((value, key) => {
-        console.log(key, value);
-    });
-
-    // Vérifiez que le token est présent
-    const authToken = localStorage.getItem('authToken');
-    if (!authToken) {
-        errorMessage.textContent = 'Vous devez être connecté pour ajouter un projet.';
-        return;
-    }
-
-    try {
-        const response = await fetch("http://localhost:5678/api/works", {
-            method: 'POST',
-            headers: {
-                'Authorization': `Bearer ${authToken}`
-            },
-            body: formData
-        });
-
-        // Log de la réponse brute
-        console.log('Response status:', response.status);
-        console.log('Response headers:', response.headers);
-
-        if (response.ok) {
-            const newProjet = await response.json();
-            console.log('New project:', newProjet); // Log des données du nouveau projet
-            // On ajoute le nouveau projet à la galerie sur la page d'accueil
-            ajouterProjets([newProjet]); 
-            // On ferme la modale après l'ajout réussi
-            modal.style.display = 'none'; 
-            // On recharge la page pour afficher le nouveau projet
-            window.location.reload();
-        } else if (response.status === 400) {
-            const error = await response.json();
-            console.error('Bad Request:', error);
-            errorMessage.textContent = 'Requête incorrecte. Veuillez vérifier les données envoyées.';
-        } else if (response.status === 401) {
-            console.error('Unauthorized:', await response.json());
-            errorMessage.textContent = 'Vous n\'êtes pas autorisé à effectuer cette action.';
-        } else if (response.status === 500) {
-            console.error('Server Error:', await response.text());
-            errorMessage.textContent = 'Erreur du serveur. Veuillez réessayer plus tard.';
-        } else {
-            const error = await response.json();
-            console.error('Other Error:', error);
-            errorMessage.textContent = error.message;
-        }
-    } catch (error) {
-        console.error('Catch Error:', error);
-        errorMessage.textContent = 'Une erreur est survenue. Veuillez réessayer plus tard.';
-    }
-}
--- FrontEnd/utils.js
+++ FrontEnd/utils.js
@@ -45,3 +45,198 @@ export function ajouterProjets(projets) {
         gallery.appendChild(figure);
     });
 }
+
+// Fonction pour afficher l'état 1 de la modale
+export function afficherEtat1(modalContent, addPictureBtn, backModalBtn) {
+    // On nettoie le contenu
+    modalContent.innerHTML = '';
+
+    // On ajoute le titre
+    const titre = document.createElement('h2');
+    titre.textContent = 'Galerie photo';
+    modalContent.appendChild(titre);
+
+    // On ajoute la div container pour les photos
+    const photosContainer = document.createElement('div');
+    photosContainer.classList.add('photos-container');
+    modalContent.appendChild(photosContainer);
+
+    // On ajoute les images des projets
+    fetchProjets().then(projets => {
+        projets.forEach(projet => {
+            const projetWrapper = document.createElement('div');
+            projetWrapper.classList.add('projet-wrapper');
+
+            const img = document.createElement('img');
+            img.src = projet.imageUrl;
+            img.alt = projet.title;
+
+            const deleteBtn = document.createElement('button');
+            deleteBtn.classList.add('delete-btn');
+            deleteBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
+
+            // Ajout de l'écouteur d'événement pour la suppression
+            deleteBtn.addEventListener('click', async () => {
+                await supprimerProjet(projet.id, projetWrapper);
+            });
+
+            projetWrapper.appendChild(img);
+            projetWrapper.appendChild(deleteBtn);
+            photosContainer.appendChild(projetWrapper);
+        });
+    });
+
+    // Mettre à jour le texte du bouton
+    addPictureBtn.textContent = 'Ajouter une photo';
+    // Cacher le bouton de retour
+    backModalBtn.style.display = 'none';
+
+    // On s'assure de ne pas avoir plusieurs écouteurs
+    addPictureBtn.removeEventListener('click', formSubmit);
+    addPictureBtn.addEventListener('click', () => {
+        afficherEtat2(modalContent, backModalBtn, addPictureBtn);
+    });
+}
+
+// Fonction pour afficher l'état 2 de la modale
+export function afficherEtat2(modalContent, backModalBtn, addPictureBtn, modal) {
+    // On nettoie le contenu
+    modalContent.innerHTML = '';
+
+    // On ajoute le titre
+    const titre = document.createElement('h2');
+    titre.textContent = 'Ajout photo';
+    modalContent.appendChild(titre);
+
+    // On affiche le bouton de retour
+    backModalBtn.style.display = 'block';
+
+    // On ajoute le formulaire d'ajout de photo
+    const form = document.createElement('form');
+    form.classList.add('modal-form');
+    form.innerHTML = `
+        <div class="photo">
+            <i class="fa-regular fa-image"></i>
+            <label for="image"><button>+ Ajouter photo</button></label>
+            <p>jpg, png : 4mo max</p>
+        </div>
+        <input type="file" id="image" name="image" accept="image/*">
+        <label for="title" class="label">Titre:</label>
+        <input type="text" id="title" name="title">
+        <label for="category" class="label">Catégorie:</label>
+        <select id="category" name="category">
+            <option value="">Sélectionnez une catégorie</option>
+        </select>
+        <p class="error-message" style="color:red;"></p>
+    `;
+    modalContent.appendChild(form);
+
+    // Récupérer les catégories et les ajouter au dropdown
+    fetchCategories().then(categories => {
+        const selectCategorie = form.querySelector('#category');
+        categories.forEach(categorie => {
+            const option = document.createElement('option');
+            option.value = categorie.id;
+            option.textContent = categorie.name;
+            selectCategorie.appendChild(option);
+        });
+    });
+
+    // Ajoutez un gestionnaire d'événement pour le bouton "Ajouter photo"
+    const addPhotoBtn = form.querySelector('.photo button');
+    addPhotoBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        form.querySelector('#image').click();
+    });
+
+    // Mettre à jour le texte du bouton
+    addPictureBtn.textContent = 'Valider';
+
+    addPictureBtn.removeEventListener('click', formSubmit);
+    addPictureBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        formSubmit(form, modal);
+    });
+}
+
+// Gestion de la suppression des projets dans la modale(Etat1)
+export async function supprimerProjet(projetId, projetWrapper) {
+    try {
+        const response = await fetch(`http://localhost:5678/api/works/${projetId}`, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('authToken')}`
+            }
+        });
+
+        if (response.ok) {
+            // Supprimer l'élément de projet du DOM
+            projetWrapper.remove();
+            // Mettre à jour la liste des projets sur la page d'accueil
+            const projets = await fetchProjets();
+            ajouterProjets(projets);
+        } else {
+            console.error("Erreur lors de la suppression du projet:", await response.json());
+        }
+    } catch (error) {
+        console.error("Erreur lors de la suppression du projet:", error);
+    }
+}
+
+// Gestion de la soumission du formulaire
+export async function formSubmit(form, modal) {
+    const formData = new FormData(form);
+    const errorMessage = form.querySelector('.error-message');
+    errorMessage.textContent = ''; // Réinitialise le message d'erreur
+
+    // On vérifie que tous les champs soient bien remplis
+    if (!formData.get('image') || !formData.get('title') || !formData.get('category')) {
+        errorMessage.textContent = 'Veuillez remplir tous les champs.';
+        return;
+    }
+
+    // Vérifiez si le token est présent dans localStorage
+    const authToken = localStorage.getItem('authToken');
+    if (!authToken) {
+        errorMessage.textContent = 'Vous devez être connecté pour ajouter un projet.';
+        return;
+    }
+
+    try {
+        // Vérifiez que le token est bien passé dans l'en-tête Authorization
+        console.log('Authorization:', `Bearer ${authToken}`);
+
+        const response = await fetch("http://localhost:5678/api/works", {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${authToken}`
+            },
+            body: formData
+        });
+
+        console.log('Response:', response);
+
+        if (response.ok) {
+            const newProjet = await response.json();
+            // On ajoute le nouveau projet à la galerie sur la page d'accueil
+            ajouterProjets([newProjet]); 
+            // On ferme la modale après l'ajout réussi
+            modal.style.display = 'none'; 
+            // On recharge la page pour afficher le nouveau projet
+            window.location.reload();
+        } else {
+            if (response.status === 400) {
+                errorMessage.textContent = 'Requête incorrecte. Veuillez vérifier les données envoyées.';
+            } else if (response.status === 401) {
+                errorMessage.textContent = 'Vous n\'êtes pas autorisé à effectuer cette action.';
+            } else if (response.status === 500) {
+                errorMessage.textContent = 'Erreur du serveur. Veuillez réessayer plus tard.';
+            } else {
+                const error = await response.json();
+                errorMessage.textContent = error.message;
+            }
+        }
+    } catch (error) {
+        errorMessage.textContent = 'Une erreur est survenue. Veuillez réessayer plus tard.';
+    }
+}
